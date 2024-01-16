7b33b4b6b8dcaaabdca9250f573f3ec0eccdaa1e
gokulhansv <gokulhansv@gmail.com>
2024-01-16 15:04:48 +0530

    new

---
+++ commands.md
@@ -0,0 +1,6 @@
+npm create vite@latest part1 -- --template react
+
+cd part1
+npm install
+npm run dev
+
---
+++ part1/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const App = () => {
+
+  return (
+    <>
+      <div>counter </div>
+    </>
+  );
+};
+
+export default App;
